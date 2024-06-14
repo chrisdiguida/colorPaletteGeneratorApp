5d41abb44de72b94f584ecb8bf7da7493fed01c6
christian.diguida@soprasteria.com <christian.diguida@soprasteria.com>
2024-06-14 20:38:26 +0200

    Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,26 @@
-const BASE_URL = 'https://localhost:5000/api/';
+const BASE_URL = 'https://api-color-palette-generator.azurewebsites.net/api/';
 const APP_USERS_CONTROLLER = `${BASE_URL}appUsers/`;
+const PALETTE_GENERATOR_CONTROLLER = `${BASE_URL}paletteGenerator/`;
 
 export const environment = {
   production: true,
 
   api: {
     base: BASE_URL,
+    appUsers: {
+      signIn: `${APP_USERS_CONTROLLER}signIn`,
+      signUp: `${APP_USERS_CONTROLLER}signUp`,
+      getCurrentAppUser: `${APP_USERS_CONTROLLER}getCurrentAppUser`,
+    },
+    palettes: {
+      generatePalette: `${PALETTE_GENERATOR_CONTROLLER}generatePalette`,
+      getAll: `${PALETTE_GENERATOR_CONTROLLER}getAll`,
+      create: `${PALETTE_GENERATOR_CONTROLLER}create`,
+      updateName: `${PALETTE_GENERATOR_CONTROLLER}updateName`,
+      delete: `${PALETTE_GENERATOR_CONTROLLER}delete`,
+    },
+  },
+  localStorageNames: {
+    token: 'token',
   },
 };
